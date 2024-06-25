73a8b98c4bdf274efbdacaadd9669962042ade2c
Megan <megan.nyakina@student.moringaschool.com>
2024-06-25 10:18:24 +0300

    adds arrow functions

---
+++ index.js
@@ -0,0 +1,30 @@
+const divide = function(){
+
+    return 2000/100;};
+
+    console.log(divide(2000/100)); 
+   
+
+// divide divides 2000 by 100
+
+
+//has an arrow function called square
+
+ const square = (x) =>{
+    return x * x;
+ };
+ console.log(square(2));
+
+
+
+
+
+
+
+//has an arrow function called add
+
+
+const add =(parameter1, parameter2) => parameter1 + parameter2;
+add(4,3); //=>7
+//add arrow function takes two parameters and adds them together
+
